9eb436cc2e7792e9b13485428f57be2a604eee29
Todd Bryant <hello@toddaas.com>
2023-07-04 17:25:29 +1000

    testing pipeline

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-// import { StaticSiteStack } from '../lib/staticsite-stack';
+import { StaticSiteStack } from '../lib/staticsite-stack';
 import { PipelineStack } from '../lib/pipeline-stack';
-import { AwsSolutionsChecks } from 'cdk-nag';
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
 
 const app = new cdk.App();
+const pipelineStack = new PipelineStack(app, 'PipelineStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+});
 
 // ## Commented out as is deloyed via CDK pipelines
 // new StaticSiteStack(app, 'StaticSiteStack', {
@@ -19,3 +22,10 @@ const app = new cdk.App();
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({
   verbose: true
 }));
+
+// CDK Nag config suppress CDK pipelines warnings
+NagSuppressions.addStackSuppressions(pipelineStack, [
+  { id: 'AwsSolutions-S1', reason: 'Suppress CDK pipelines warnings' },
+  { id: 'AwsSolutions-IAM5', reason: 'Suppress CDK pipelines warnings' },
+  { id: 'AwsSolutions-CB4', reason: 'Suppress CDK pipelines warnings' },
+]);
--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -8,27 +8,28 @@ export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // This creates a new CodeCommit repository called 'WorkshopRepo'
-    const repo = new codecommit.Repository(this, 'WorkshopRepo', {
-        repositoryName: "WorkshopRepo"
-    });
-
     // The basic pipeline declaration. This sets the initial structure
     // of our pipeline
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'WorkshopPipeline',
+      pipelineName: 'Pipeline',
       synth: new CodeBuildStep('SynthStep', {
-          input: CodePipelineSource.codeCommit(repo, 'main'),
+          input: CodePipelineSource.connection('toddsfish/toomanycontainers', 'main', {
+            connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:302766791300:connection/853f1d55-685e-46d6-af0a-d7256c1052aa'
+          }),
           installCommands: [
               'npm install -g aws-cdk'
           ],
           commands: [
+              'cd ./infra',
               'npm ci',
               'npm run build',
               'npx cdk synth'
           ]
         }
-      )
+      ),
+      /* selfMutation: This needs to be set to true to allow the pipeline to reconfigure itself when assets or stages are being added to it, and true is the recommended setting.
+      You can temporarily set this to false while you are iterating on the pipeline itself and prefer to deploy changes using cdk deploy. */
+      selfMutation: true
     });
     
   }
